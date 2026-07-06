65ce8c85861f7cb575f825a40d4ba4047418a77a
Liam Reimers <liam@shurloc.com>
2026-07-06 14:07:22 -0700

    Add PHPUnit testing infrastructure

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * PHPUnit bootstrap.
+ *
+ * @package ShurLocProductTools
+ */
+
+declare( strict_types=1 );
+
+require_once dirname( __DIR__ ) . '/includes/models/class-shurloc-mesh-specification.php';
+require_once dirname( __DIR__ ) . '/includes/parsers/class-shurloc-mesh-parser.php';
