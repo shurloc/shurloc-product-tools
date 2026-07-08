ad35c4b847e69e52b309b24022b28d66b982c943
Liam Reimers <liam@shurloc.com>
2026-07-08 12:53:29 -0700

    Update the plugin version to 0.3.0 in the constants.php file.

--- includes/constants.php
+++ includes/constants.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'SHURLOC_PRODUCT_TOOLS_VERSION', '0.1.0' );
+define( 'SHURLOC_PRODUCT_TOOLS_VERSION', '0.3.0' );
 
 /**
  * Plugin directory path.
