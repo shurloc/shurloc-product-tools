487e716c88396ba42e3caf6ace8fcde552ff5c45
Liam Reimers <liam@shurloc.com>
2026-07-21 09:20:14 -0700

    Introduce Shurloc_Mesh_Product_Table_Shortcode.

---
+++ includes/shortcodes/class-shurloc-mesh-product-table-shortcode.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Mesh product table shortcode.
+ *
+ * Provides the [shurloc_mesh_table] shortcode.
+ *
+ * @package ShurLocProductTools
+ */
+
+declare( strict_types=1 );
+
+/**
+ * Mesh product table shortcode.
+ */
+final class Shurloc_Mesh_Product_Table_Shortcode {
+
+	/**
+	 * Mesh product data service.
+	 *
+	 * @var Shurloc_Mesh_Product_Data_Service_Interface
+	 */
+	private Shurloc_Mesh_Product_Data_Service_Interface $data_service;
+
+	/**
+	 * Mesh product table renderer.
+	 *
+	 * @var Shurloc_Mesh_Product_Table_Renderer_Interface
+	 */
+	private Shurloc_Mesh_Product_Table_Renderer_Interface $renderer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Shurloc_Mesh_Product_Data_Service_Interface   $data_service Mesh product data service.
+	 * @param Shurloc_Mesh_Product_Table_Renderer_Interface $renderer     Table renderer.
+	 */
+	public function __construct(
+		Shurloc_Mesh_Product_Data_Service_Interface $data_service,
+		Shurloc_Mesh_Product_Table_Renderer_Interface $renderer
+	) {
+
+		$this->data_service = $data_service;
+		$this->renderer     = $renderer;
+	}
+
+	/**
+	 * Register shortcode.
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+
+		add_shortcode(
+			'shurloc_mesh_table',
+			array(
+				$this,
+				'render',
+			)
+		);
+	}
+
+	/**
+	 * Render shortcode.
+	 *
+	 * @param array<string,mixed> $attributes Shortcode attributes.
+	 * @return string
+	 */
+    // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found, Squiz.Commenting.FunctionComment.Missing
+	public function render(
+		array $attributes = array()
+	): string {
+
+		global $product;
+
+		if ( ! $product instanceof WC_Product ) {
+			return '';
+		}
+
+		$result = $this->data_service->analyze_product(
+			$product
+		);
+
+		if ( ! $result->is_mesh_product() ) {
+			return '';
+		}
+
+		return $this->renderer->render(
+			$result
+		);
+	}
+}
