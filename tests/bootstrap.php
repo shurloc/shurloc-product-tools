aa3d6e5543701cb32e3f527103ca8815edf9c50b
Liam Reimers <liam@shurloc.com>
2026-07-28 12:57:46 -0700

    Introduce Shurloc_Product_Schema_Service_Double

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -59,4 +59,5 @@ require_once dirname( __DIR__ ) . '/tests/doubles/class-shurloc-mesh-product-ana
 require_once dirname( __DIR__ ) . '/tests/doubles/class-shurloc-mesh-product-table-renderer.php';
 require_once dirname( __DIR__ ) . '/tests/doubles/class-shurloc-mesh-product-data-service.php';
 require_once dirname( __DIR__ ) . '/tests/doubles/class-shurloc-mesh-product-table-shortcode.php';
+require_once dirname( __DIR__ ) . '/tests/doubles/class-shurloc-product-schema-service.php';
 require_once dirname( __DIR__ ) . '/tests/doubles/class-wp-post.php';
