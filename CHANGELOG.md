87a30dba4c42c683413b1c0ccbb00ee2fd23a931
Liam Reimers <liam@shurloc.com>
2026-08-03 12:12:48 -0700

    Bump version to 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [1.0.1] - 2026-08-03
+
+### Changed
+
+- Changed product schema mainEntityOfPage to reflect webPage node's @id postfix of #webpage.
+
 ## [1.0.0] - 2026-07-30
 
 ### Added
--- includes/constants.php
+++ includes/constants.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Plugin version.
  */
-define( 'SHURLOC_PRODUCT_TOOLS_VERSION', '1.0.0' );
+define( 'SHURLOC_PRODUCT_TOOLS_VERSION', '1.0.1' );
 
 /**
  * Plugin directory path.
