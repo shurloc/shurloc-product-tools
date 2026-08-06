bce25b4294dcb496c21f6aabf0074d26c3bb5538
Liam Reimers <liam@shurloc.com>
2026-08-06 13:13:44 -0700

    Introduce Shurloc_Dynamic_Cross_Sells and tests, and update infrastructure to support it

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -145,10 +145,16 @@ function shurloc_product_tools_bootstrap(): void {
 	$breadcrumb_separator->register();
 
 	$recommendation_eligibility = new Shurloc_Product_Recommendation_Eligibility_Service();
-	$related_products           = new Shurloc_Related_Products(
+
+	$related_products = new Shurloc_Related_Products(
 		$recommendation_eligibility
 	);
 	$related_products->register();
+
+	$dynamic_cross_sells = new Shurloc_Dynamic_Cross_Sells(
+		$recommendation_eligibility
+	);
+	$dynamic_cross_sells->register();
 }
 
 add_action(
