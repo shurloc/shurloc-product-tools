d5f58ed66ee3e4a646f23b8f691f1656c067d71c
Liam Reimers <liam@shurloc.com>
2026-08-10 09:39:51 -0700

    Add PHPStan bootstrap for level 1 correctness

---
+++ tests/phpstan-bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * PHPStan bootstrap.
+ *
+ * @package ShurLocProductTools
+ */
+
+declare( strict_types=1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/../' );
+}
+
+require_once __DIR__ . '/../includes/constants.php';
