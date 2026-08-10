4795c845ca7cdda8172ca6296ce436504bafa4d2
Liam Reimers <liam@shurloc.com>
2026-08-10 13:39:27 -0700

    Fix get_variation_attributes in Shurloc_Test_WC_Product_Variation and WC_Product_Variation

--- tests/doubles/class-shurloc-test-wc-product-variation.php
+++ tests/doubles/class-shurloc-test-wc-product-variation.php
@@ -47,10 +47,13 @@ if ( ! class_exists( 'Shurloc_Test_WC_Product_Variation' ) ) {
 		/**
 		 * Get variation attributes.
 		 *
-		 * @return array<string,string>
+		 * @param mixed $with_prefix Prependattribute_ or not.
+		 * @return array<string|int,mixed>
 		 */
-		public function get_variation_attributes(): array {
-
+		public function get_variation_attributes(
+			mixed $with_prefix = true
+		): array {
+			unset( $with_prefix );
 			return $this->test_variation_attributes;
 		}
 
