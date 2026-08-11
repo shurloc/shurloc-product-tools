a655c825757178d38fd8a438e8e69f2446117bcc
Liam Reimers <liam.reimers@gmail.com>
2026-08-11 08:07:38 -0700

    Update PHPStan bootstrap for SHURLOC_PRODUCT_TOOLS_URL type correctness

--- tests/phpstan-bootstrap.php
+++ tests/phpstan-bootstrap.php
@@ -11,4 +11,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', __DIR__ . '/../' );
 }
 
-require_once __DIR__ . '/../includes/constants.php';
+if ( ! defined( 'SHURLOC_PRODUCT_TOOLS_VERSION' ) ) {
+	define(
+		'SHURLOC_PRODUCT_TOOLS_VERSION',
+		'0.1.0'
+	);
+}
+
+if ( ! defined( 'SHURLOC_PRODUCT_TOOLS_PATH' ) ) {
+	define(
+		'SHURLOC_PRODUCT_TOOLS_PATH',
+		__DIR__ . '/'
+	);
+}
+
+if ( ! defined( 'SHURLOC_PRODUCT_TOOLS_URL' ) ) {
+	define(
+		'SHURLOC_PRODUCT_TOOLS_URL',
+		'https://example.com/wp-content/plugins/shurloc-product-tools/'
+	);
+}
