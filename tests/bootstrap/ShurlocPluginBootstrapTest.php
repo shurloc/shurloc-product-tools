06c4068db6f27ce13d79705138d1a5d5e8bb2337
Liam Reimers <liam@shurloc.com>
2026-08-27 12:01:19 -0700

    Add bootstrap bits and test for feature

--- tests/bootstrap/ShurlocPluginBootstrapTest.php
+++ tests/bootstrap/ShurlocPluginBootstrapTest.php
@@ -19,14 +19,17 @@ final class ShurlocPluginBootstrapTest extends TestCase {
 
 	/**
 	 * Set up bootstrap test environment.
+	 *
+	 * @return void
 	 */
 	protected function setUp(): void {
 
 		parent::setUp();
 
-		$GLOBALS['shurloc_test_actions'] = array();
-
+		$GLOBALS['shurloc_test_actions']         = array();
 		$GLOBALS['shurloc_test_action_metadata'] = array();
+		$GLOBALS['shurloc_test_filters']         = array();
+		$GLOBALS['shurloc_test_filter_metadata'] = array();
 	}
 
 	/**
@@ -89,6 +92,50 @@ final class ShurlocPluginBootstrapTest extends TestCase {
 			class_exists(
 				Shurloc_Autoloader::class
 			)
+		);
+	}
+
+	/**
+	 * Bootstrap should register primary product category services.
+	 *
+	 * @return void
+	 */
+	public function test_bootstrap_registers_primary_product_category_services(): void {
+
+		$this->load_plugin();
+
+		shurloc_product_tools_bootstrap();
+
+		$this->assertArrayHasKey(
+			'add_meta_boxes_product',
+			$GLOBALS['shurloc_test_actions']
+		);
+
+		$this->assertArrayHasKey(
+			'save_post_product',
+			$GLOBALS['shurloc_test_actions']
+		);
+
+		$this->assertArrayHasKey(
+			'admin_enqueue_scripts',
+			$GLOBALS['shurloc_test_actions']
+		);
+
+		$this->assertArrayHasKey(
+			'wpseo_primary_term_taxonomies',
+			$GLOBALS['shurloc_test_filters']
+		);
+
+		$this->assertSame(
+			11,
+			$GLOBALS['shurloc_test_filter_metadata']
+			['wpseo_primary_term_taxonomies'][0]['priority']
+		);
+
+		$this->assertSame(
+			3,
+			$GLOBALS['shurloc_test_filter_metadata']
+			['wpseo_primary_term_taxonomies'][0]['accepted_args']
 		);
 	}
 
